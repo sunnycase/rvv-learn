620da0252185878a9a265b3a4e641ace68bcafc3
sunnycase <sunnycase@live.cn>
2022-04-05 18:31:13 +0800

    small 2.7GFlops

--- main.cpp
+++ main.cpp
@@ -104,246 +104,129 @@ void method_2(size_t M, size_t K, size_t N, const float *A, const float *B,
 
 void method_3(size_t M, size_t K, size_t N, const float *A, const float *B,
               const float *Bias, float *C, float min, float max) {
+  const size_t UNROLL = 8;
   const float *ptr_a = A;
   float *ptr_out = C;
   size_t vl = 0;
-  auto NN = N / 4;
-  auto REMAIN = N - NN * 4;
-  for (size_t m = 0; m < M; m++) {
-    const float *pb = B;
-    float *pc = ptr_out;
-    const float *pbias = Bias;
-    for (size_t nn = NN; nn; nn -= vl) {
-      vl = vsetvl_e32m4(nn);
-      const float *pa = ptr_a;
-      const float *pb_vl = pb;
-
-      // init acc with bias
-      auto acc0 = vle32_v_f32m4(pbias + vl * 0, vl);
-      auto acc1 = vle32_v_f32m4(pbias + vl * 1, vl);
-      auto acc2 = vle32_v_f32m4(pbias + vl * 2, vl);
-      auto acc3 = vle32_v_f32m4(pbias + vl * 3, vl);
-
-      for (size_t k = 0; k < K; k++) {
-        auto vb0 = vle32_v_f32m4(pb_vl + vl * 0, vl);
-        auto vb1 = vle32_v_f32m4(pb_vl + vl * 1, vl);
-        auto vb2 = vle32_v_f32m4(pb_vl + vl * 2, vl);
-        auto vb3 = vle32_v_f32m4(pb_vl + vl * 3, vl);
-
-        acc0 = vfmacc_vf_f32m4(acc0, *pa, vb0, vl);
-        acc1 = vfmacc_vf_f32m4(acc1, *pa, vb1, vl);
-        acc2 = vfmacc_vf_f32m4(acc2, *pa, vb2, vl);
-        acc3 = vfmacc_vf_f32m4(acc3, *pa, vb3, vl);
-        pb_vl += N;
-        pa++;
-      }
-
-      // update acc with act
-      acc0 = vfmin_vf_f32m4(acc0, max, vl);
-      acc1 = vfmin_vf_f32m4(acc1, max, vl);
-      acc2 = vfmin_vf_f32m4(acc2, max, vl);
-      acc3 = vfmin_vf_f32m4(acc3, max, vl);
-
-      acc0 = vfmax_vf_f32m4(acc0, min, vl);
-      acc1 = vfmax_vf_f32m4(acc1, min, vl);
-      acc2 = vfmax_vf_f32m4(acc2, min, vl);
-      acc3 = vfmax_vf_f32m4(acc3, min, vl);
-
-      vse32_v_f32m4(pc + vl * 0, acc0, vl);
-      vse32_v_f32m4(pc + vl * 1, acc1, vl);
-      vse32_v_f32m4(pc + vl * 2, acc2, vl);
-      vse32_v_f32m4(pc + vl * 3, acc3, vl);
-
-      pb += vl;
-      pc += vl;
-      pbias += vl;
-    }
-
-    vl = 0;
-    pb = B + NN * 4;
-    pc = ptr_out + NN * 4;
-    pbias = Bias + NN * 4;
-    for (size_t n = REMAIN; n; n -= vl) {
-      vl = vsetvl_e32m8(n);
-      const float *pa = ptr_a;
-      const float *pb_vl = pb;
-
-      // init acc with bias
-      auto acc = vle32_v_f32m8(pbias, vl);
-
-      for (size_t k = 0; k < K; k++) {
-        auto vb = vle32_v_f32m8(pb_vl, vl);
-        acc = vfmacc_vf_f32m8(acc, *pa++, vb, vl);
-        pb_vl += N;
-      }
-
-      // update acc with act
-      acc = vfmax_vf_f32m8(vfmin_vf_f32m8(acc, max, vl), min, vl);
-
-      vse32_v_f32m8(pc, acc, vl);
-      pb += vl;
-      pc += vl;
-      pbias += vl;
-    }
-
-    ptr_a += K;
-    ptr_out += N;
-  }
-}
-
-void method_4(size_t M, size_t K, size_t N, const float *A, const float *B,
-              const float *Bias, float *C, float min, float max) {
-  const float *ptr_a = A;
-  float *ptr_out = C;
-  size_t vl = 0;
-  auto NN = N / 8;
-  auto REMAIN = N - NN * 8;
-  for (size_t m = 0; m < M; m++) {
-    const float *pb = B;
-    float *pc = ptr_out;
-    const float *pbias = Bias;
-    for (size_t nn = NN; nn; nn -= vl) {
-      vl = vsetvl_e32m1(nn);
-      const float *pa = ptr_a;
-      const float *pb_vl = pb;
-
-      // init acc with bias
-      auto acc0 = vle32_v_f32m1(pbias + vl * 0, vl);
-      auto acc1 = vle32_v_f32m1(pbias + vl * 1, vl);
-      auto acc2 = vle32_v_f32m1(pbias + vl * 2, vl);
-      auto acc3 = vle32_v_f32m1(pbias + vl * 3, vl);
-      auto acc4 = vle32_v_f32m1(pbias + vl * 4, vl);
-      auto acc5 = vle32_v_f32m1(pbias + vl * 5, vl);
-      auto acc6 = vle32_v_f32m1(pbias + vl * 6, vl);
-      auto acc7 = vle32_v_f32m1(pbias + vl * 7, vl);
-
-      for (size_t k = 0; k < K; k++) {
-        auto vb0 = vle32_v_f32m1(pb_vl + vl * 0, vl);
-        auto vb1 = vle32_v_f32m1(pb_vl + vl * 1, vl);
-        auto vb2 = vle32_v_f32m1(pb_vl + vl * 2, vl);
-        auto vb3 = vle32_v_f32m1(pb_vl + vl * 3, vl);
-        auto vb4 = vle32_v_f32m1(pb_vl + vl * 4, vl);
-        auto vb5 = vle32_v_f32m1(pb_vl + vl * 5, vl);
-        auto vb6 = vle32_v_f32m1(pb_vl + vl * 6, vl);
-        auto vb7 = vle32_v_f32m1(pb_vl + vl * 7, vl);
-
-        acc0 = vfmacc_vf_f32m1(acc0, *pa, vb0, vl);
-        acc1 = vfmacc_vf_f32m1(acc1, *pa, vb1, vl);
-        acc2 = vfmacc_vf_f32m1(acc2, *pa, vb2, vl);
-        acc3 = vfmacc_vf_f32m1(acc3, *pa, vb3, vl);
-        acc4 = vfmacc_vf_f32m1(acc0, *pa, vb4, vl);
-        acc5 = vfmacc_vf_f32m1(acc1, *pa, vb5, vl);
-        acc6 = vfmacc_vf_f32m1(acc2, *pa, vb6, vl);
-        acc7 = vfmacc_vf_f32m1(acc3, *pa, vb7, vl);
-        pb_vl += N;
-        pa++;
-      }
-
-      // update acc with act
-      acc0 = vfmin_vf_f32m1(acc0, max, vl);
-      acc1 = vfmin_vf_f32m1(acc1, max, vl);
-      acc2 = vfmin_vf_f32m1(acc2, max, vl);
-      acc3 = vfmin_vf_f32m1(acc3, max, vl);
-      acc4 = vfmin_vf_f32m1(acc4, max, vl);
-      acc5 = vfmin_vf_f32m1(acc5, max, vl);
-      acc6 = vfmin_vf_f32m1(acc6, max, vl);
-      acc7 = vfmin_vf_f32m1(acc7, max, vl);
-
-      acc0 = vfmax_vf_f32m1(acc0, min, vl);
-      acc1 = vfmax_vf_f32m1(acc1, min, vl);
-      acc2 = vfmax_vf_f32m1(acc2, min, vl);
-      acc3 = vfmax_vf_f32m1(acc3, min, vl);
-      acc4 = vfmax_vf_f32m1(acc4, min, vl);
-      acc5 = vfmax_vf_f32m1(acc5, min, vl);
-      acc6 = vfmax_vf_f32m1(acc6, min, vl);
-      acc7 = vfmax_vf_f32m1(acc7, min, vl);
-
-      vse32_v_f32m1(pc + vl * 0, acc0, vl);
-      vse32_v_f32m1(pc + vl * 1, acc1, vl);
-      vse32_v_f32m1(pc + vl * 2, acc2, vl);
-      vse32_v_f32m1(pc + vl * 3, acc3, vl);
-      vse32_v_f32m1(pc + vl * 0, acc4, vl);
-      vse32_v_f32m1(pc + vl * 1, acc5, vl);
-      vse32_v_f32m1(pc + vl * 2, acc6, vl);
-      vse32_v_f32m1(pc + vl * 3, acc7, vl);
-
-      pb += vl;
-      pc += vl;
-      pbias += vl;
-    }
-
-    vl = 0;
-    pb = B + NN * 8;
-    pc = ptr_out + NN * 8;
-    pbias = Bias + NN * 8;
-    for (size_t n = REMAIN; n; n -= vl) {
-      vl = vsetvl_e32m1(n);
-      const float *pa = ptr_a;
-      const float *pb_vl = pb;
-
-      // init acc with bias
-      auto acc = vle32_v_f32m1(pbias, vl);
-
-      for (size_t k = 0; k < K; k++) {
-        auto vb = vle32_v_f32m1(pb_vl, vl);
-        acc = vfmacc_vf_f32m1(acc, *pa++, vb, vl);
-        pb_vl += N;
-      }
-
-      // update acc with act
-      acc = vfmax_vf_f32m1(vfmin_vf_f32m1(acc, max, vl), min, vl);
-
-      vse32_v_f32m1(pc, acc, vl);
-      pb += vl;
-      pc += vl;
-      pbias += vl;
-    }
-
-    ptr_a += K;
-    ptr_out += N;
-  }
-}
-
-void method_5(size_t M, size_t K, size_t N, const float *A, const float *B,
-              const float *Bias, float *C, float min, float max) {
-  const float *ptr_a = A;
-  float *ptr_out = C;
-  size_t vl = 0;
-  size_t MM = M / 2;
-  size_t REAMIN = M - MM * 2;
+  size_t MM = M / UNROLL;
+  size_t REAMIN = M - MM * UNROLL;
   for (size_t mm = 0; mm < MM; mm++) {
     const float *pb = B;
     float *pc = ptr_out;
     const float *pbias = Bias;
     for (size_t n = N; n; n -= vl) {
-      vl = vsetvl_e32m8(n);
+      vl = vsetvl_e32m2(n);
       const float *pa0 = ptr_a;
       const float *pa1 = pa0 + K;
+      const float *pa2 = pa0 + K * 2;
+      const float *pa3 = pa0 + K * 3;
+      const float *pa4 = pa0 + K * 4;
+      const float *pa5 = pa0 + K * 5;
+      const float *pa6 = pa0 + K * 6;
+      const float *pa7 = pa0 + K * 7;
       const float *pb_vl = pb;
 
       // init acc with bias
-      auto acc0 = vle32_v_f32m8(pbias, vl);
+      auto acc0 = vle32_v_f32m2(pbias, vl);
       auto acc1 = acc0;
+      auto acc2 = acc0;
+      auto acc3 = acc0;
+      auto acc4 = acc0;
+      auto acc5 = acc0;
+      auto acc6 = acc0;
+      auto acc7 = acc0;
 
       for (size_t k = 0; k < K; k++) {
-        auto vb = vle32_v_f32m8(pb_vl, vl);
-        acc0 = vfmacc_vf_f32m8(acc0, *pa0++, vb, vl);
-        acc1 = vfmacc_vf_f32m8(acc1, *pa1++, vb, vl);
+        auto vb = vle32_v_f32m2(pb_vl, vl);
+        acc0 = vfmacc_vf_f32m2(acc0, *pa0++, vb, vl);
+        acc1 = vfmacc_vf_f32m2(acc1, *pa1++, vb, vl);
+        acc2 = vfmacc_vf_f32m2(acc2, *pa2++, vb, vl);
+        acc3 = vfmacc_vf_f32m2(acc3, *pa3++, vb, vl);
+        acc4 = vfmacc_vf_f32m2(acc4, *pa4++, vb, vl);
+        acc5 = vfmacc_vf_f32m2(acc5, *pa5++, vb, vl);
+        acc6 = vfmacc_vf_f32m2(acc6, *pa6++, vb, vl);
+        acc7 = vfmacc_vf_f32m2(acc7, *pa7++, vb, vl);
         pb_vl += N;
       }
 
       // update acc with act
-      acc0 = vfmax_vf_f32m8(vfmin_vf_f32m8(acc0, max, vl), min, vl);
-      acc1 = vfmax_vf_f32m8(vfmin_vf_f32m8(acc1, max, vl), min, vl);
+      acc0 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc0, max, vl), min, vl);
+      acc1 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc1, max, vl), min, vl);
+      acc2 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc2, max, vl), min, vl);
+      acc3 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc3, max, vl), min, vl);
+      acc4 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc4, max, vl), min, vl);
+      acc5 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc5, max, vl), min, vl);
+      acc6 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc6, max, vl), min, vl);
+      acc7 = vfmax_vf_f32m2(vfmin_vf_f32m2(acc7, max, vl), min, vl);
 
-      vse32_v_f32m8(pc + K * 0, acc0, vl);
-      vse32_v_f32m8(pc + K * 1, acc1, vl);
+      vse32_v_f32m2(pc + K * 0, acc0, vl);
+      vse32_v_f32m2(pc + K * 1, acc1, vl);
+      vse32_v_f32m2(pc + K * 2, acc2, vl);
+      vse32_v_f32m2(pc + K * 3, acc3, vl);
+      vse32_v_f32m2(pc + K * 4, acc4, vl);
+      vse32_v_f32m2(pc + K * 5, acc5, vl);
+      vse32_v_f32m2(pc + K * 6, acc6, vl);
+      vse32_v_f32m2(pc + K * 7, acc7, vl);
       pb += vl;
       pc += vl;
       pbias += vl;
     }
-    ptr_a += K * 2;
-    ptr_out += N;
+    ptr_a += K * UNROLL;
+    ptr_out += N * UNROLL;
+  }
+}
+
+void method_small(size_t M, size_t K, size_t N, const float *A, const float *B,
+                  const float *Bias, float *C, float min, float max) {
+  const size_t UNROLL = 4;
+  const float *ptr_a = A;
+  float *ptr_out = C;
+  size_t vl = 0;
+  size_t MM = M / UNROLL;
+  size_t REAMIN = M - MM * UNROLL;
+  for (size_t mm = 0; mm < MM; mm++) {
+    const float *pb = B;
+    float *pc = ptr_out;
+    const float *pbias = Bias;
+    for (size_t n = N; n; n -= vl) {
+      vl = vsetvl_e32m4(n);
+      const float *pa0 = ptr_a;
+      const float *pa1 = pa0 + K;
+      const float *pa2 = pa0 + K * 2;
+      const float *pa3 = pa0 + K * 3;
+      const float *pb_vl = pb;
+
+      // init acc with bias
+      auto acc0 = vle32_v_f32m4(pbias, vl);
+      auto acc1 = vle32_v_f32m4(pbias, vl);
+      auto acc2 = vle32_v_f32m4(pbias, vl);
+      auto acc3 = vle32_v_f32m4(pbias, vl);
+
+      for (size_t k = 0; k < K; k++) {
+        auto vb = vle32_v_f32m4(pb_vl, vl);
+        acc0 = vfmacc_vf_f32m4(acc0, *pa0++, vb, vl);
+        acc1 = vfmacc_vf_f32m4(acc1, *pa1++, vb, vl);
+        acc2 = vfmacc_vf_f32m4(acc2, *pa2++, vb, vl);
+        acc3 = vfmacc_vf_f32m4(acc3, *pa3++, vb, vl);
+        pb_vl += N;
+      }
+
+      // update acc with act
+      acc0 = vfmax_vf_f32m4(vfmin_vf_f32m4(acc0, max, vl), min, vl);
+      acc1 = vfmax_vf_f32m4(vfmin_vf_f32m4(acc1, max, vl), min, vl);
+      acc2 = vfmax_vf_f32m4(vfmin_vf_f32m4(acc2, max, vl), min, vl);
+      acc3 = vfmax_vf_f32m4(vfmin_vf_f32m4(acc3, max, vl), min, vl);
+
+      vse32_v_f32m4(pc + K * 0, acc0, vl);
+      vse32_v_f32m4(pc + K * 1, acc1, vl);
+      vse32_v_f32m4(pc + K * 2, acc2, vl);
+      vse32_v_f32m4(pc + K * 3, acc3, vl);
+      pb += vl;
+      pc += vl;
+      pbias += vl;
+    }
+    ptr_a += K * UNROLL;
+    ptr_out += N * UNROLL;
   }
 }
 
@@ -364,8 +247,19 @@ std::unique_ptr<float[]> randmat(size_t M, size_t N) {
 struct test_method {
   method_t method;
   const char *name;
-} methods[] = {
-    {method_ref, "ref"}, {method_1, "m1"}, {method_2, "m2"}, {method_3, "m3"}, {method_4, "m4"}, {method_5, "m5"}};
+} methods[] = {{method_ref, "ref"},
+               {method_1, "m1"},
+               {method_2, "m2"},
+               {method_3, "m3"},
+               {method_small, "small"}};
+
+void print_vl(size_t n) {
+  std::cout << "\nVL for K=" << n << std::endl;
+  std::cout << "m1\t\t" << vsetvl_e32m1(n) << std::endl;
+  std::cout << "m2\t\t" << vsetvl_e32m2(n) << std::endl;
+  std::cout << "m4\t\t" << vsetvl_e32m4(n) << std::endl;
+  std::cout << "m8\t\t" << vsetvl_e32m8(n) << std::endl;
+}
 
 void test(size_t scale, size_t M, size_t K, size_t N) {
 
@@ -380,22 +274,26 @@ void test(size_t scale, size_t M, size_t K, size_t N) {
   auto C = std::make_unique<float[]>(M * N);
   auto RefC = std::make_unique<float[]>(M * N);
   auto min = randfloat();
-  auto max = randfloat();
+  auto max = min + 1.f;
   auto MAC = M * N * K;
+  auto MEMORY = (M * K + K * N + M * N + N) * sizeof(float);
+
+  print_vl(K);
 
   // ref
   methods[0].method(M, K, N, A.get(), B.get(), Bias.get(), RefC.get(), min,
                     max);
 
   std::cout << "\nTESTING M=" << M << " K=" << K << " N=" << N << " MAC=" << MAC
+            << " MEMORY=" << MEMORY << " DENSITY=" << ((float)MAC / MEMORY)
             << std::endl;
 
   for (size_t i = 0; i < sizeof(methods) / sizeof(test_method); i++) {
-    auto &m = methods[i];
-    memset(C.get(), M * N * sizeof(float), 0);
+    auto &method = methods[i];
+    memset(C.get(), 0, M * N * sizeof(float));
     // warm up
     for (size_t j = 0; j < WARM_UP_COUNT * scale; j++) {
-      m.method(M, K, N, A.get(), B.get(), Bias.get(), C.get(), min, max);
+      method.method(M, K, N, A.get(), B.get(), Bias.get(), C.get(), min, max);
     }
 
     // check
@@ -403,28 +301,31 @@ void test(size_t scale, size_t M, size_t K, size_t N) {
       for (size_t n = 0; n < N; n++) {
         auto diff = std::abs(C[m * N + n] - RefC[m * N + n]);
         if (diff > std::numeric_limits<float>::epsilon()) {
-          std::cerr << "Check failed: " << methods[i].name << " M=" << m
-                    << " N=" << n << std::endl;
-          throw 1;
+          std::cerr << "Check failed: " << method.name << " M=" << m
+                    << " N=" << n << " Diff=" << diff << std::endl;
+          goto test_b;
         }
       }
     }
 
+  test_b:
     // test
     auto t1 = high_resolution_clock::now();
     auto test_count = TEST_COUNT * scale;
     for (size_t j = 0; j < test_count; j++) {
-      m.method(M, K, N, A.get(), B.get(), Bias.get(), C.get(), min, max);
+      method.method(M, K, N, A.get(), B.get(), Bias.get(), C.get(), min, max);
     }
     auto t2 = high_resolution_clock::now();
     duration<double, std::milli> ms_double = t2 - t1;
     auto t = ms_double.count() / test_count;
-    std::cout << m.name << "\t\t" << t << "ms"
-              << "\t\t" << (MAC / t / 1e6) << "GMACs" << std::endl;
+    std::cout << method.name << "\t\t" << t << "ms"
+              << "\t\t" << (MAC / t / 1e6) << "GMACs"
+              << "\t\t" << (MEMORY / t / 1e6) << "GB" << std::endl;
   }
 }
 
 int main() {
   test(20, 16, 16, 16);
+  test(20, 16, 32, 32);
   test(1, 16, 128, 128);
 }
